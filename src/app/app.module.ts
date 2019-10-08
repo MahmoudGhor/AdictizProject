1fa019d92283c7fd8d98890ea3f07b06a8743e53
mghorbel <mahmoud.ghorbel@esprit.tn>
2019-10-08 21:12:49 +0100

    create result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchComponent } from './search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatInputModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+} from '@angular/material';
+import { ResultComponent } from './result/result.component';
+import {BookApiService} from './service/book-api.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    MatProgressSpinnerModule
   ],
-  providers: [],
+  providers: [BookApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
